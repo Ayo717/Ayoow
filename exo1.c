408ae49e87fdb5c8d2ee2b68805d7c2e1ab6c53e
Ayo717 <Ayoub.Sellamicherkaoui@emsi-edu.ma>
2026-01-02 21:22:12 +0100

    Update exo1.c

--- exo1.c
+++ exo1.c
@@ -4,9 +4,9 @@ int main() {
     int N, i, j, k;
     printf("Entrez N : ");
     scanf("%d", &N);
-    for (i = 1; i <= N; i++) {  // Boucle lignes
-        for (k = 1; k <= N - i; k++) printf(" ");  // Nested: espaces
-        for (j = 1; j <= 2 * i - 1; j++) printf("*");  // Nested: étoiles
+    for (i = 1; i <= N; i++) {  
+        for (k = 1; k <= N - i; k++) printf(" ");  
+        for (j = 1; j <= 2 * i - 1; j++) printf("*"); 
         printf("\n");
     }
     return 0;
