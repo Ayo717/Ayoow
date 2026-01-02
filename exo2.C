9a443ff01651ebd37039dad49daf0de91ca84b5c
Ayo717 <Ayoub.Sellamicherkaoui@emsi-edu.ma>
2026-01-02 21:21:14 +0100

    Update exo2.C

--- exo2.C
+++ exo2.C
@@ -2,7 +2,7 @@
 
 int est_premier(int n) {
     if (n <= 1) return 0;
-    for (int i = 2; i * i <= n; i++)
+    for (int i = 2; i * i <= n; i++)  // Nested pour diviseurs
         if (n % i == 0) return 0;
     return 1;
 }
