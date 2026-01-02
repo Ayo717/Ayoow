c23f27b69409bd3e9b4b87a78673c5bf9914bcb5
Ayo717 <Ayoub.Sellamicherkaoui@emsi-edu.ma>
2026-01-02 21:20:27 +0100

    Update exo3.C

--- exo3.C
+++ exo3.C
@@ -1,40 +1,19 @@
 #include <stdio.h>
 
-int estBissextile(int annee) {
-    if ((annee % 4 == 0 && annee % 100 != 0) || (annee % 400 == 0))
-        return 1;
-    else
-        return 0;
+int est_premier(int n) {
+    if (n <= 1) return 0;
+    for (int i = 2; i * i <= n; i++)
+        if (n % i == 0) return 0;
+    return 1;
 }
 
 int main() {
-    int j, m, a;
-    int maxJours;
-
-    printf("Donner une date (j/m/an) : ");
-    scanf("%d/%d/%d", &j, &m, &a);
-
-    if (a <= 0 || m < 1 || m > 12 || j < 1 || j > 31) {
-        printf("Date non valide.\n");
-        return 0;
+    int N;
+    printf("Entrez N : ");
+    scanf("%d", &N);
+    for (int i = 2; i <= 2 * N; i++) {
+        if (est_premier(i)) printf("%d ", i);
     }
-
-    if (m == 2) {
-        if (estBissextile(a))
-            maxJours = 29;
-        else
-            maxJours = 28;
-    } else if (m == 4 || m == 6 || m == 9 || m == 11) {
-        maxJours = 30;
-    } else {
-        maxJours = 31;
-    }
-
-    if (j >= 1 && j <= maxJours)
-        printf("Date valide.\n");
-    else
-        printf("Date non valide.\n");
-
+    printf("\n");
     return 0;
 }
-
