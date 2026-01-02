bd8657f13a5262e91b5556789efe1c16098e6d28
Ayo717 <Ayoub.Sellamicherkaoui@emsi-edu.ma>
2026-01-02 21:21:22 +0100

    Update exo3.C

--- exo3.C
+++ exo3.C
@@ -2,7 +2,7 @@
 
 int est_premier(int n) {
     if (n <= 1) return 0;
-    for (int i = 2; i * i <= n; i++)
+    for (int i = 2; i * i <= n; i++)  // Nested loop
         if (n % i == 0) return 0;
     return 1;
 }
@@ -11,7 +11,7 @@ int main() {
     int N;
     printf("Entrez N : ");
     scanf("%d", &N);
-    for (int i = 2; i <= 2 * N; i++) {
+    for (int i = 2; i <= 2 * N; i++) {  // Externe: nombres
         if (est_premier(i)) printf("%d ", i);
     }
     printf("\n");
